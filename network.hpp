2f5a71f5f0605647e14551fbefca2eee7a2e6a4f
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-04-09 14:54:09 -0400

    Update network.hpp

--- network.hpp
+++ network.hpp
@@ -1,3 +1,11 @@
 #include <vector>
+#include <exception>
 using namespace std;
-
+class network {
+public:
+class public std::exception: NeuralNetworkLockedError {
+public:
+    char* err;
+    char* what(){return err;}
+};
+};
