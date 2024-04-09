a794c397c23c0db05370a95f1a5159b932e9cca1
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-04-09 14:58:06 -0400

    Update network.hpp

--- network.hpp
+++ network.hpp
@@ -2,8 +2,10 @@
 #include <exception>
 using namespace std;
 class network {
+private:
+bool isInit = false; // Turns true when user initializes neural network
 public:
-class public std::exception: NeuralNetworkLockedError {
+class public std::exception: NeuralNetworkInitializedError {
 public:
     char* err;
     char* what(){return err;}
